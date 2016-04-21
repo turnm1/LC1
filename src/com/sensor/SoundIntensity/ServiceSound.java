93b8002c1fb068d5cf859f6b6d68e1e3c8ecb513
Turna <Turna@MeteNOVO.bfh.ch>
2016-04-21 22:44:28 +0200

    ServiceSound verbunden mit Broker

--- src/com/sensor/SoundIntensity/ServiceSound.java
+++ src/com/sensor/SoundIntensity/ServiceSound.java
@@ -7,20 +7,75 @@ package com.sensor.SoundIntensity;
 
 import com.tinkerforge.BrickletSoundIntensity;
 import com.tinkerforge.IPConnection;
+import com.communication.MQTTCommunication;
+import com.communication.MQTTParameters;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 /**
  *
  * @author Turna
  */
-public class ServiceSound {
+public class ServiceSound implements MqttCallback {
+
+    public final static String CLIENT_ID="XYZ";
     
-    private static final String HOST = "localhost";
+    public final static String BASE_TOPIC = "Service";
+    public final static String STATUS_TOPIC = BASE_TOPIC + "/status";
+    
+    public final static String STATUS_TOPIC_CONNECTION = STATUS_TOPIC + "/connection";
+    public final static String STATUS_CONNECTION_OFFLINE="offline";
+    public final static String STATUS_CONNECTION_ONLINE="online";
+    
+    private final MQTTCommunication communication;
+    
+        private static final String HOST = "localhost";
 	private static final int PORT = 4223;
 	private static final String UID = "mnn"; // Change to your UID
+    
+     public ServiceSound() throws MqttException {
+        communication = new MQTTCommunication();
+        MQTTParameters parameters = new MQTTParameters();
+        parameters.setClientID(CLIENT_ID);
+        parameters.setIsCleanSession(false);
+        parameters.setIsLastWillRetained(true);
+        parameters.setLastWillMessage(STATUS_CONNECTION_OFFLINE.getBytes());
+        parameters.setLastWillQoS(1);
+        parameters.setServerURIs(URI.create("tcp://127.0.0.1:1883"));
+        parameters.setWillTopic(STATUS_TOPIC_CONNECTION);
+        parameters.setMqttCallback(this);
+        communication.connect(parameters);
+        communication.publishActualWill(STATUS_CONNECTION_ONLINE.getBytes());
+        communication.subscribe(BASE_TOPIC+"/#", 0);
 
-	// Note: To make the example code cleaner we do not handle exceptions. Exceptions
-	//       you might normally want to catch are described in the documentation
-	public static void main(String args[]) throws Exception {
-		IPConnection ipcon = new IPConnection(); // Create IP connection
+    }
+
+    @Override
+    public void connectionLost(Throwable thrwbl) {
+        System.out.println("Ouups, lost connection to subscirptions");
+    }
+
+    @Override
+    public void messageArrived(String string, MqttMessage mm) throws Exception {
+        System.out.printf("Message has been delivered and is back again. Topic: %s, Message: %s \n", string, new String(mm.getPayload()));
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken imdt) {
+        System.out.println("Delivery is done.");
+    }
+
+    
+    // Note: To make the example code cleaner we do not handle exceptions. Exceptions
+    //       you might normally want to catch are described in the documentation
+    public static void main(String[] args) throws MqttException, Exception {
+        
+        ServiceSound service=new ServiceSound();
+        IPConnection ipcon = new IPConnection(); // Create IP connection
 		BrickletSoundIntensity si = new BrickletSoundIntensity(UID, ipcon); // Create device object
 
 		ipcon.connect(HOST, PORT); // Connect to brickd
@@ -40,6 +95,6 @@ public class ServiceSound {
 
 		System.out.println("Press key to exit"); System.in.read();
 		ipcon.disconnect();
-	}
+    }
     
 }
